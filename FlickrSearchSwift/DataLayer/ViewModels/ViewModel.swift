7de994274b907f4066d2cb28b8f765e0fb857d50
mujtabaalam <mujtaba.alam.khan@gmail.com>
2016-08-26 08:58:55 +0300

    Added ViewModel

---
+++ FlickrSearchSwift/DataLayer/ViewModels/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  FlickrSearchSwift
+//
+//  Created by Mujtaba Alam on 8/26/16.
+//  Copyright © 2016 Mujtaba Alam. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: NSObject {
+
+}
